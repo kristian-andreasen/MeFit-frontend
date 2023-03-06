c35365937988bf4392c0d871eb0275879a431c39
Kristian Andreasen <kristian.andreasen@fi.experis.com>
2023-03-06 14:15:29 +0100

    feature-login-page

---
+++ src/components/login/LoginForm.js
@@ -0,0 +1,29 @@
+import './LoginForm.css';
+
+function LoginForm() {
+  return (
+    <>
+      <h3>Log in</h3>
+      <form>
+        <div class="login-field">
+          <div class="row">
+            <label class="title">Username or Email</label>
+            <input type="text" id="login" />
+          </div>
+          <div class="row">
+            <label class="title">Password</label>
+            <input type="password" id="login" />
+          </div>
+        </div>
+        <div class="btn-wrapper">
+          <button id="login-btn">LOG IN</button>
+        </div>
+      </form>
+      <div class="btn-wrapper">
+        <button id="signup-btn">SIGN UP</button>
+      </div>
+    </>
+  );
+}
+
+export default LoginForm;
