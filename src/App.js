1c596b17e8d8e3c02d2dae15cf6da181cc480a29
Lass3819 <102652990+Lass3819@users.noreply.github.com>
2023-03-10 12:23:55 +0100

    put keycloak.login() and keycloak.logout() into functions that change the redirectURI before so that it properly redirects to "/profile" after logging in and "/" when logging out.

--- src/App.js
+++ src/App.js
@@ -9,14 +9,16 @@ import { ROLES } from './const/roles';
 import StartPage from './pages/StartPage';
 
 
-//<ReactKeycloakProvider authClient = {keycloak}>
 function App() {
   return (
     
       <BrowserRouter>
         <div className='App'>
+          
           <Routes>
-            <Route path="/" element={<StartPage/>}/>
+            
+            <Route path="/" element={<StartPage/>} />
+            <Route path="/profile" element={<p>profile</p>}/>
             <Route path='/login' element={
               <KeycloakRoute role={ROLES.User}>
                   {<p>successfully assigned role</p>}
