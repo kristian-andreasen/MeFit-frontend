a958c9055396e8f1d8e3f8682be47ab197c977e8
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2023-03-13 10:23:51 +0100

    refactored a whole lot of CSS

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,14 @@
-import ProfileHeader from "../components/profile/ProfileHeader";
+import ProfileHeader from '../components/profile/ProfileHeader';
+import ProfileTabs from '../components/profile/ProfileTabs';
+import ProgramsList from '../components/programs/ProgramsList';
 
-function Profile(){
-    return(
-        <ProfileHeader />
-    )
-
+function Profile() {
+  return (
+    <div className='profile-container'>
+      <ProfileHeader />
+      <ProfileTabs />
+    </div>
+  );    
 }
 
 export default Profile;
--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,36 +1,20 @@
-import keycloak, { loginWithRedirect, logoutWithRedirect } from "../keycloak";
-import Navbar from "../components/navbar/Navbar";
-import { Navigate } from "react-router-dom";
+import keycloak, { loginWithRedirect, logoutWithRedirect } from '../keycloak';
+import Navbar from '../components/navbar/Navbar';
+import { Navigate } from 'react-router-dom';
+import './StartPage.css';
 
 /**
  * Example Start Page using Keycloak Context.
  */
 function StartPage() {
   return (
-    <div>
-      <Navbar/>
-      <h1>Start Page</h1>
-
-      <section className="actions">
-        {!keycloak.authenticated && (
-          <button onClick={() => loginWithRedirect()}>Login</button>
-        )}
-        {keycloak.authenticated && (
-          <button onClick={() => logoutWithRedirect()}>Logout</button>
-        )}
-      </section>
-
-      {keycloak.token && (
-        <div>
-          <h4>Token</h4>
-          <p>Name: { keycloak.tokenParsed.name}</p>
-          <p>Username: { keycloak.tokenParsed.preferred_username}</p>
-          <p>Sub: { keycloak.tokenParsed.sub }</p>
-
-          <pre>{keycloak.token}</pre>
-          
-        </div>
-      )}
+    <div className='homepage-container'>
+      <Navbar />
+      <main className='home-main-content'>
+        <section className='banner-container'>
+          <h1 className='banner-title'>If you never do it<br></br>you will never have done it.</h1>
+        </section>
+      </main>
     </div>
   );
 }
