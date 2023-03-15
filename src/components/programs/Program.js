755bf85735bde79423bf4fe0c4c299a8887431c3
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2023-03-15 12:19:03 +0100

    created skeleton for program details page

--- src/components/programs/Program.js
+++ src/components/programs/Program.js
@@ -1,10 +1,45 @@
 import ProfileHeader from '../../pages/profile/ProfileHeader';
+import ProfileTabs from '../../pages/profile/ProfileTabs';
+
+import './Program.css';
 
 function Program() {
   return (
     <>
       <ProfileHeader />
-      <h1>Program details</h1>
+      <ProfileTabs />
+      <main className='program-details-page'>
+        <section className='program-details'>
+          <div>
+            <h1 className='program-title'>Program Title</h1>
+            <p className='program-creator'>by Arnold Schwarzenegger</p>
+          </div>
+          <p className='program-description'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <button className='start-program-button'>START TODAY</button>
+          <div className='workout-details'>
+            <p>Workouts:</p>
+            <ul className='workouts-list'>
+              <li className='workout-item'>
+                <p>Lower body focus on hamstrings</p>
+                <button>View Workout</button>
+              </li>
+              <li className='workout-item'>
+                <p>Upper body focus vertical</p>
+                <button>View Workout</button>
+              </li>
+              <li className='workout-item'>
+                <p>Whole body strength & endurance</p>
+                <button>View Workout</button>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
