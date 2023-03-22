7b5a5791be496b713c5e5079cb8698aa7370bd69
Lasse <lass3819@gmail.com>
2023-03-22 23:45:01 +0100

    working on syncing with database

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
+import keycloak from '../../keycloak';
 import './Loading.css';
 
-function Loading(props) {
+function Loading() {
+
+  useEffect(()=>{
+    if(keycloak.authenticated){
+      sessionStorage.setItem("id",keycloak.tokenParsed.sub)
+    }
+
+
+  },[])
+
+
+
+
   return (
     <div className='loading-container'>
       <h1 className='loading'>Connecting...</h1>
+      {sessionStorage.getItem("id")}
     </div>
     
   );
