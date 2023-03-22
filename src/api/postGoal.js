c153b54e4f71f73ce4da50fe7e1f9f39cf0d3cb1
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2023-03-22 12:04:03 +0100

    user can create a goal and set a name, startDate, and endDate

--- src/api/postGoal.js
+++ src/api/postGoal.js
@@ -1,6 +1,19 @@
-import apiURL from "../const/apiUrl";
+import apiURL from '../const/apiUrl';
+import keycloak from '../keycloak';
 
+export async function postGoal(goalData) {
+  const response = await fetch(`${apiURL}/goals`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${keycloak.token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(goalData),
+  });
 
-export const postGoal = async () => {
-    
+  if (!response.ok) {
+    throw new Error('Could not create goal');
+  }
+  const data = JSON.stringify(response);
+  return data;
 }
