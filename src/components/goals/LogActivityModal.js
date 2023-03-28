a6b49ba765f105912786952805d378ecaf548b1f
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2023-03-28 09:43:23 +0200

    displaying workouts related to the program as goal has subscribed to

--- src/components/goals/LogActivityModal.js
+++ src/components/goals/LogActivityModal.js
@@ -7,71 +7,83 @@ import ModalButton from '../ModalButton';
 import GoalSelect from './GoalSelect';
 
 function LogActivityModal({ handleClose }) {
-  const [selectedGoal,setSelectedGoal] = useState(null);
+  const [selectedGoal, setSelectedGoal] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
-
 
   const close = () => setModalOpen(false);
   const open = () => setModalOpen(true);
 
   const dropIn = {
-      hidden: {
+    hidden: {
       y: '-100vh',
       opacity: 0,
-      },
-      visible: {
+    },
+    visible: {
       y: '0',
       opacity: 1,
       transition: {
-          duration: 0.1,
-          type: 'spring',
-          damping: 25,
-          stiffness: 500,
+        duration: 0.1,
+        type: 'spring',
+        damping: 25,
+        stiffness: 500,
       },
-      },
-      exit: {
+    },
+    exit: {
       y: '100vh',
       opacity: 0,
-      },
+    },
   };
 
   const handleSelectChange = (selectedOption) => {
-      setSelectedGoal(selectedOption);
-      console.log(selectedGoal);
+    setSelectedGoal(selectedOption);
+    console.log(selectedOption.program.workouts);
   };
-
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-      //await deleteGoal(selectedGoal.id);
-      
-      
+    //await deleteGoal(selectedGoal.id);
   };
 
   const handleModalClick = (e) => {
-      e.stopPropagation();
+    e.stopPropagation();
   };
 
   return (
-      <Backdrop onClick={handleClose} className='modal orange-gradient'>
+    <Backdrop onClick={handleClose} className='modal orange-gradient'>
       <motion.div
-          onClick={handleModalClick}
-          className='modal orange-gradient'
-          variants={dropIn}
-          initial='hidden'
-          animate='visible'
-          exit='exit'
+        onClick={handleModalClick}
+        className='modal orange-gradient'
+        variants={dropIn}
+        initial='hidden'
+        animate='visible'
+        exit='exit'
       >
-          <h2>Select a goal to delete</h2>
-          <form onSubmit={handleSubmit} className='create-goal-form'>
-              <GoalSelect onSelect={handleSelectChange}/>
+        <h2>Select a goal to delete</h2>
+        <form onSubmit={handleSubmit} className='create-goal-form'>
+          <GoalSelect onSelect={handleSelectChange} />
+          {selectedGoal && selectedGoal.program && (
+            <ul>
+              {selectedGoal.program.workouts.map((workout) => (
+                <li key={workout.id}>
+                  <input
+                    type='checkbox'
+                    id={workout.id}
+                    name='workout'
+                    checked={workout.achieved}
+                    value={workout.id}
+                  />
+                  <label htmlFor={workout.id}>{workout.name}</label>
+                </li>
+              ))}
+            </ul>
+          )}
           <button type='submit' className='submit-button-delete'>
-              Delete
+            Log Activity
           </button>
-          </form>
-          <ModalButton onClick={handleClose} label='Close' />
+        </form>
+        <ModalButton onClick={handleClose} label='Close' />
       </motion.div>
-      </Backdrop>
+    </Backdrop>
   );
 }
 
